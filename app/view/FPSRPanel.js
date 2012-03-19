522a3c30840f15dc38d92fc88aece17ca4b715d7
Gerard Gutierrez <gerard.gutierrez@estudiants.urv.cat>
2012-03-19 10:35:48 +0100

    carregar estat a l'inici

--- app/view/FPSRPanel.js
+++ app/view/FPSRPanel.js
@@ -134,8 +134,7 @@ Ext.define('Painometer.view.FPSRPanel', {
                     },
                     {
                         xtype: 'InfoContainer',
-                        id: 'FPSInfoCont',
-                        itemId: 'FPSInfoContainer'
+                        id: 'FPSInfoCont'
                     }
                 ]
             }

6409c4269ba229e496fef93b4ca7d8dd51c30c09
Roman Roset <roman.roset@urv.cat>
2012-03-30 11:43:38 +0200

    fix some errors

--- app/view/VASPanel.js
+++ app/view/VASPanel.js
@@ -26,6 +26,7 @@ Ext.define('Painometer.view.VASPanel', {
         layout: {
             type: 'fit'
         },
+        zIndex : 0,
         items: [
             {
                 xtype: 'container',
